9b05e10d8e7c17efcbb20dd32f399a1a748608d6
unknown <eng.alaaebasyony@gmail.com>
2022-02-09 22:00:46 +0200

    updates

---
+++ app/Models/Slider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Slider extends Model
+{
+    //
+    protected $table = 'sliders';
+    public $timestamps = true;
+    protected $fillable = array('name', 'url' ,'image');
+}
